e30ba121d6407c1b0e33363aa0439d468e7b28bd
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-07-18 15:13:12 +0200

    ajout de commentaires

--- public/app.js
+++ public/app.js
@@ -1,47 +1,59 @@
-	var tabQuestions= [];
+var tabQuestions= [];
 
-	$.ajax('/Question').done(function(question){
-		for (var i = 0; i < question.length; i++) {
-			tabQuestions.push(question[i]);
+//requête ajax au chargement de la page
+$.ajax('/Question').done(function(question){
+	for (var i = 0; i < question.length; i++) {
+		tabQuestions.push(question[i]);
+	}
+});
+
+//écoute les click de class next dans l'élément body, même si les boutons sont généré dynamiquement
+$('body').on('click', '.next', function (){
+//récupère la valeur associé aux boutons et la transforme en type number
+	var quest = parseInt($(this).val());
+//vide le body a chaque click
+	$('#body').empty();
+//crée des TH avec des données de la BDD
+	$('#body').append('<table><thead><tr><th>Question '+tabQuestions[quest].ID+'</th></tr><tr><th>Question '+tabQuestions[quest].question+'</th></tr></thead><tbody id="questionnaire" ></tbody>');
+
+//crée des TD avec des données de la BDD||| Boutons généré avec une class rep!!!!
+	$('#questionnaire').append('<tr><td><button class="rep">'+tabQuestions[quest].reponse1+'</button></td><td><button class="rep">'+tabQuestions[quest].reponse2+'</button></td></tr>');
+	$('#questionnaire').append('<tr><td><button class="rep">'+tabQuestions[quest].reponse3+'</button></td><td><button class="rep">'+tabQuestions[quest].reponse4+'</button></td></tr>');
+//écoute les click de class rep dans l'élément body, même si les boutons sont généré dynamiquement
+	$('body').on('click', '.rep', function(){
+//récupère ce que contient le bouton click
+		var reponse = $(this).html();
+
+//désactive tout les boutons de class rep quand l'un d'eux est cliqué
+		$('.rep').prop('disabled', true);
+//compare le bouton cliqué avec la réponse
+		if (reponse == 'La France') {
+//change la couleur du bouton cliqué en vert si c'est juste
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Paris') {
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Les Monténégrins') {
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Entre 1914 et 1918') {
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Le 15 août 1769') {
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Des fruits et des céréales, pour vous sentir en pleine forme.') {
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Que pour la mafia.') {
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Hulk') {
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Mark Zuckerberg') {
+			$(this).css('background-color', 'green');
+		}else if (reponse == 'Link') {
+			$(this).css('background-color', 'green');
+		}else{
+//si le bouton clické est faux change la couleur en rouge
+			$(this).css('background-color', 'red');
 		}
 	});
-
-	$('body').on('click', '.next', function (){
-		var quest = parseInt($(this).val());
-
-		$('#body').empty();
-		$('#body').append('<table><thead><tr><th>Question '+tabQuestions[quest].ID+'</th></tr><tr><th>Question '+tabQuestions[quest].question+'</th></tr></thead><tbody id="questionnaire" ></tbody>');
-
-		$('#questionnaire').append('<tr><td><button class="rep">'+tabQuestions[quest].reponse1+'</button></td><td><button class="rep">'+tabQuestions[quest].reponse2+'</button></td></tr>');
-		$('#questionnaire').append('<tr><td><button class="rep">'+tabQuestions[quest].reponse3+'</button></td><td><button class="rep">'+tabQuestions[quest].reponse4+'</button></td></tr>');
-		$('body').on('click', '.rep', function(){
-			var reponse = $(this).html();
-
-			$('.rep').prop('disabled', true);
-			if (reponse == 'La France') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Paris') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Les Monténégrins') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Entre 1914 et 1918') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Le 15 août 1769') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Des fruits et des céréales, pour vous sentir en pleine forme.') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Que pour la mafia.') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Hulk') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Mark Zuckerberg') {
-				$(this).css('background-color', 'green');
-			}else if (reponse == 'Link') {
-				$(this).css('background-color', 'green');
-			}else{
-				$(this).css('background-color', 'red');
-			}
-		});
-		$('#body').append('<button value="'+(quest-1)+'" class="next" >Question suivante</button>');
-		$('#body').append('<button value="'+(quest+1)+'" class="next" >Question suivante</button>');
-	});
+//crée les boutons suivant et précédent
+	$('#body').append('<button value="'+(quest-1)+'" class="next" >Question précédente</button>');
+	$('#body').append('<button value="'+(quest+1)+'" class="next" >Question suivante</button>');
+});
--- serveur.js
+++ serveur.js
@@ -1,3 +1,4 @@
+//chargement des librairies JS
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
@@ -5,29 +6,40 @@ var app = express();
 
 var question = [];
 
+//charge les fichiers static qui sont dans le dossier .../public
+//ces fichiers seront accessible a l'adresse .../questionnaire
 app.use('/questionnaire', express.static(__dirname + '/public'));
 
+//initialisation de body-parser qui sert a parser les json échangé avec app.js
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+//définis le port ou il faut se connecté
 app.listen(8080, function () {
 	console.log('Connection sur le port 8080!');
 });
 
+//création des paramètres de connection pour la base de donnée
 var connection = mysql.createConnection({
-	host: 'localhost',
-	user: '#',
-	password: '#',
-	database: 'Questionnaire'
+	host: 'x',
+	user: 'x',
+	password: 'x',
+	database: 'x'
 });
 
+//connection a la base de données avec les paramètres définis précédemment
 connection.connect(function(err) {
+//if err sert a catcher les erreurs
 	if (err) throw err
 		console.log('You are now connected...');
+//fait une requête SQL a la base de donnée
 	connection.query('SELECT * FROM Questions', function(err, results) {
 		if (err) throw err
+			//parcours le tableau results qui est renvoyé par la base de donnée
 			for (var i = 0; i < results.length; i++) {
+//push des objets du tableau results qui ont donc les clé de la BDD
+				// question.push(results[i]);
+//création d'objet avec de nouvelles clés et push dans le tableau question
 				question.push({
 					ID : results[i].Id,
 					question : results[i].Question,
@@ -41,6 +53,7 @@ connection.connect(function(err) {
 });
 
 
+//envois le tableau question en json quand une requête ajax est fait sur cette URL
 app.get('/Question', function(req, res) {
 	res.json(question);
 	console.log(question);
